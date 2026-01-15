4b896c226e2f911db7f9e0dc5cbd6015c89b47a2
Hunter <HunterBradshaw2@outlook.com>
2026-01-14 20:23:49 -0600

    addded error support

--- app.py
+++ app.py
@@ -14,5 +14,17 @@ def projects():
 def linktree():
     return render_template('linktree.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error.html', 
+                           error_code=404, 
+                           error_message="We couldn't find the page you were looking for."), 404
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('error.html', 
+                           error_code=500, 
+                           error_message="Our server is having a moment. Please try again later."), 500
+
 if __name__ == '__main__':
     app.run(debug=True)
